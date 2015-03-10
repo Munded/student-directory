d534482fc0a2fa1b64338912b11385ea79175a37
Edmund O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-03-10 17:27:29 +0000

    VERSION 6.0

--- directory.rb
+++ directory.rb
@@ -1,18 +1,26 @@
 
 
-#put all students into an array
-students = [
-  {:name => "Arin Hanson", :cohort => :february},
-  {:name => "Stan Smith", :cohort => :february},
-  {:name => "Dan Evidan", :cohort => :february},
-  {:name => "Rick Grimes", :cohort => :march},
-  {:name => "Clint Eastwood", :cohort => :march},
-  {:name => "Charlie Sheen", :cohort => :march},
-  {:name => "Gavin Free", :cohort => :december},
-  {:name => "Michael Jones", :cohort => :december},
-  {:name => "Ed O'Brien", :cohort => :december}
-  ]
 
+
+#student input code
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  #get first name
+  name = gets.chomp
+  #while name is not empty, repeat code
+  while !name.empty? do
+    # add the student hash to array
+    students << {:name => name, :cohort => :november}
+     puts "Now we have #{students.length} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
+end
 
 
 # then print array, with methods and shit
@@ -33,6 +41,7 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 #Gotta call those methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
