4823989dd096904caf83a553e2828b47b85ccb8f
Edmund O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-03-10 16:52:43 +0000

    Version 3.1

--- directory.rb
+++ directory.rb
@@ -1,17 +1,30 @@
 
 student_count = 9
-#print list of students
+#put all students into an array
+students = [
+  "Arin Hanson",
+  "Stan Smith",
+  "Dan Evidan",
+  "Rick Grimes",
+  "Clint Eastwood",
+  "Charlie Sheen",
+  "Gavin Free",
+  "Michael Jones",
+  "Ed O'Brien",
+  ]
+
+# then print array
 puts "The student of my cohort at Makers Academy"
 puts "---------------------------------"
-puts "Arin Hanson"
-puts "Stan Smith"
-puts "Dan Evidan"
-puts "Rick Grimes"
-puts "Clint Eastwood"
-puts "Charlie Sheen"
-puts "Gavin Free"
-puts "Michael Jones"
-puts "Ed O'Brien"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
 
 #error can occur here if use "" and '', causing miscommunication to ruby
 
