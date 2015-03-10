5948ed0ac0e9f43411777dd4e27b7f4183c8d13c
Edmund O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-03-10 18:42:36 +0000

    Refratored and Reorganizedgit add -u V8

--- directory.rb
+++ directory.rb
@@ -1,70 +1,72 @@
-#interaction menu
-def interactive_menu
-  students = []
-  loop do
-  #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-  #2 read the input and save it into a variable
-    selection = gets.chomp
-  #3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9" 
-      exit 
-    else 
-      puts "Me no understandy"
-  end
-end
-end
+@students = []
 
-
-
-
-#student input code
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
-  #get first name
-  name = gets.chomp
-  #while name is not empty, repeat code
-  while !name.empty? do
-    # add the student hash to array
-    students << {:name => name, :cohort => :november}
-     puts "Now we have #{students.length} students"
-    # get another name from the user
-    name = gets.chomp
-  end
-  students
-end
-
-
-# then print array, with methods and shit
 def print_header
   puts "The student of my cohort at Makers Academy"
   puts "---------------------------------"
 end
 
-  def print(students)
-    students.each_with_index do |student, i|
+def print_student_list
+    @students.each_with_index do |student, i|
       puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-#error can occur here if use "" and '', causing miscommunication to ruby
-#print total
-def print_footer(names)  
-  puts "Overall, we have #{names.length} great students"
+def print_footer
+  puts "Overall, we have #{@students.length} great students"
 end
-#Gotta call those methods
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  #get first name
+  name = gets.chomp
+  #while name is not empty, repeat code
+  while !name.empty? do
+    # add the student hash to array
+    @students << {:name => name, :cohort => :november}
+     puts "Now we have #{@students.length} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  @students
+end
+
+
+
+
+def print_menu
+  puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+case selection
+      when "1"
+        input_students
+      when "2"
+        show_students  
+      when "9" 
+        exit 
+    else 
+      puts "Me no understandy"
+  end
+end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+end
+end
+
 
 interactive_menu
